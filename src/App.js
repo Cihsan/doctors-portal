039d378117721acbcd407338e47e8c5c28a1938a
Cihsan <mutamuty@gmail.com>
2022-05-10 10:20:04 +0600

    set all page route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './Pages/Common/Header';
+import Header from './Common/Header';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Review from './Pages/Review/Review';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/appointment'element={<Appointment/>}></Route>
+        <Route path='/reviews'element={<Review/>}></Route>
+        <Route path='/contact'element={<Contact/>}></Route>
+        <Route path='/login'element={<Login/>}></Route>
+      </Routes>
     </div>
   );
 }
