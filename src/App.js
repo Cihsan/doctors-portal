38e5c58d69e432631d4c3994b5fb1d5b66cfbe85
Cihsan <mutamuty@gmail.com>
2022-05-10 09:21:49 +0600

    tailwind and daiysiUI added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This first</h1>
+      <button class="btn w-64 rounded-full">Button</button>
+    </div>
+  );
+}
+
+export default App;
